80c8354dd6b6a5cef0d1cfbe6e162268647baed6
tomo666 <ulyssesrecords@hotmail.com>
2024-09-10 16:35:29 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,10 +27,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://tomo666.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tomo666/gamedevlab/',
+  baseUrl: '/gamedevlab/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
